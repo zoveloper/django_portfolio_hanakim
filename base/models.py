753c83944c4b6e0fe3ef85082b8458b968ff3bf3
Martin Cho <martincho@Martins-MacBook-Pro.local>
2021-04-15 23:47:19 -0400

    added CRUD Fuction

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+    thumbnail = models.ImageField(null=True, blank=True, default='/placeholder.png')
+    category = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    active = models.BooleanField(default=False)
+    featured = models.BooleanField(default=False)
+    id = models.AutoField( editable=False, primary_key=True) 
+
+    def __str__(self):
+        return self.name
--- base/views.py
+++ base/views.py
@@ -1,16 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+from .forms import PostForm
+
+from .models import Post
 # Create your views here.
 
 def home(request):
-    return render(request,'base/index.html')
+    posts = Post.objects.filter(active=True)[0:20]
 
-def posts(request):
-    return render(request,'base/posts.html')
+    context = {'posts':posts}
+    return render(request,'base/index.html',context)
 
-def post(request):
-    return render(request,'base/post.html')
+def posts(request, pk):
+    posts = Post.objects.get(id=pk)
+
+    context = {'posts':posts}
+    return render(request,'base/posts.html', context)
+
+def post(request, pk):
+    post=Post.objects.get(id=pk)
+
+    context = {'post':post}
+    return render(request,'base/post.html', context)
 
 def profile(request):
     return render(request,'base/profile.html')
+
+#CRUD VIEWS
+@login_required(login_url="home")
+def createPost(request):
+    form = PostForm
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+
+    context = {'form':form}
+    return render(request,'base/post_form.html', context)
+
+@login_required(login_url="home")
+def updatePost(request, pk):
+    post = Post.objects.get(id=pk)
+    form = PostForm(instance=post)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+
+    context = {'form':form}
+    return render(request,'base/post_form.html', context)
