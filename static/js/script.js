eab7fd8b068f8ccdc337c608b1376e1629e94da2
Martin Cho <zoveloper@gmail.com>
2021-04-18 01:16:06 -0400

    adding Responsive NavBar

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,8 @@
-console.log('its good day')
+const toggleBtn = document.querySelector('.navbar_toggleBtn');
+const mainNav = document.querySelector('.main_nav');
+
+toggleBtn.addEventListener('click', () => {
+    mainNav.classList.toggle('active');
+});
+
+console.log(toggleBtn)
