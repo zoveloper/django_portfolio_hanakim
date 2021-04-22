a33c758d82d327fd93a2d9233ecd26176967c200
Martin Cho <zoveloper@gmail.com>
2021-04-21 23:00:44 -0400

    Added Scroll back to Top btn

--- static/js/script.js
+++ static/js/script.js
@@ -24,11 +24,21 @@ document.querySelector('#contact-form')
         toggleModal();
     });
 
-    /*
-document.getElementById('submit-btn')
-    .addEventListener('submit', (event) =>{
-        event.preventDefault();
-        toggleModal();
-    });
+    var mybutton = document.getElementById("myBtn");
 
-*/
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+    
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
