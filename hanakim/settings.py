15c14ba1c19ad47992c5a019834cf9786c3c6ff8
Martin Cho <zoveloper@gmail.com>
2021-04-23 19:42:06 -0400

    setting.py changed

--- hanakim/settings.py
+++ hanakim/settings.py
@@ -116,8 +116,7 @@ WSGI_APPLICATION = 'hanakim.wsgi.application'
 #     }
 # }
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES = { 'default': dj_database_url.config() }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
